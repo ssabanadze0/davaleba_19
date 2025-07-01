948e067036771850feca45e38378a09e8b3ac075
ssabanadze0 <ssabanadze0@gmail.com>
2025-07-01 15:46:56 +0400

    it was fun

--- app.js
+++ app.js
@@ -11,3 +11,40 @@ img.src =
 img.alt = "Random image";
 img.classList.add("myImage");
 container.after(img);
+//////
+
+const section = document.getElementById("characters-list");
+const html = characters
+  .map((character) => {
+    return `
+      <div class="character-card">
+        <img src="${character.image}" alt="${character.first_name} ${character.last_name}">
+        <h2>${character.first_name} ${character.last_name}</h2>
+        <p><strong>House:</strong> ${character.house}</p>
+        <button class="delete-btn">Delete</button>
+        <button class="info-btn">Info</button>
+        <p class="actor-info" style="display: none;"><strong>Actor:</strong> ${character.actor}</p>
+      </div>
+    `;
+  })
+  .join("");
+
+section.innerHTML = html;
+///////
+const deleteButtons = document.querySelectorAll(".delete-btn");
+const infoButtons = document.querySelectorAll(".info-btn");
+
+deleteButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.remove();
+  });
+});
+
+infoButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const actorInfo = btn.parentElement.querySelector(".actor-info");
+
+    actorInfo.style.display =
+      actorInfo.style.display === "none" ? "block" : "none";
+  });
+});
